4d9c7dfdcb06a58e3cc345e701f6ba7eecb39720
LeedsByExample <leeda.minkin@yahoo.com>
2023-09-25 12:39:00 -0500

    Completed Part 2 Candidate Testing Autograded. Was super fantastic about it.

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,9 +12,22 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+
+
+let questions = 
+  ["Who was the first American woman in space? ", 
+  "True or false: 5 kilometer == 5000 meters? ", 
+  "(5 + 3)/2 * 10 = ? ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+  "What is the minimum crew size for the ISS? " ];
+let correctAnswers = 
+  ["Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"];
+let candidateAnswers = [];
+
 
 
 function askForName() {
@@ -24,18 +37,31 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-    candidateAnswer = input.question(question);
+    // candidateAnswer = input.question(question);
+    for (let i = 0; i < 5; i++) {
+      candidateAnswers.push(input.question(questions[i]));
+      // console.log(candidateAnswers); //was checking to make sure candidates answers were being pushed into the candidateAnswers array
+   }
 }
+
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-    if (candidateAnswer === correctAnswer) {
-      console.log("That is correct. Congratulations.")
-    } else {
-      console.log("I'm sorry, that is incorrect.");
-    }
+    // if (candidateAnswer === correctAnswer) {
+    //   console.log("That is correct. Congratulations.")
+    // } else {
+    //   console.log("I'm sorry, that is incorrect.");
+    // }
 
+    for (i = 0; i < candidateAnswers.length; i++) {
+      if (candidateAnswers[i] === correctAnswers[i]) {
+        console.log(`You answered: ${candidateAnswers[i]}. Congratulations! ${candidateAnswers[i]} is the correct answer. `);
+      } else {
+        console.log(`You answered: ${candidateAnswers[i]}. That is incorrect. The correct answer is ${correctAnswers[i]}.`);
+      }
+    }
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -46,9 +72,9 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Nice to meet you, " + candidateName);
+   console.log("Nice to meet you, " + candidateName + ". Let's begin.");
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  gradeQuiz(candidateAnswers);
 }
 
 runProgram();
@@ -64,3 +90,7 @@ module.exports = {
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
+
+//unsure again why getting undefined error
+//okay i think its because candidateAnswers is a parameter of function gradeQuiz? is that necessary?
+//can i use a variable that's been declared/initialized outside of the function as a parameter of the function? i don't think so?
